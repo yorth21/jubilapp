f50a4cb8c07f0ed846904eab32c14011b897bc7a
christian <cristianyamith@hotmail.com>
2025-02-28 00:50:56 -0500

    feat: add tailwind conf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/features/home/home.component';
+import { ModuloOnlyComponent } from './components/component/modulo-only/modulo-only.component';
+import { ButtonExitComponent } from './components/component/button-exit/button-exit.component';
+import { IconJubilComponent } from './components/component/icon-jubil/icon-jubil.component';
+import { H1HeaderComponent } from './components/component/h1-header/h1-header.component';
+import { ActivePhoteComponent } from './components/component/active-phote/active-phote.component';
+import { WelcomeComponent } from './components/features/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: WelcomeComponent,
+  },
+];
