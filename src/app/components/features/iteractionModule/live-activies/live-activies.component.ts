1e653e69d342675a853938502bffa6d9636f4448
christian <cristianyamith@hotmail.com>
2025-03-11 23:47:01 -0500

    feat: add changes in activity live

--- src/app/components/features/iteractionModule/live-activies/live-activies.component.ts
+++ src/app/components/features/iteractionModule/live-activies/live-activies.component.ts
@@ -2,39 +2,46 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { Activity } from '../../../../modules/shared/models/activity';
+import { ModuloOnlyComponent } from '../../../component/modulo-only/modulo-only.component';
 
 @Component({
   selector: 'app-live-activies',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModuloOnlyComponent],
   templateUrl: './live-activies.component.html',
   styleUrl: './live-activies.component.css',
 })
 export class LiveActiviesComponent {
   constructor(private router: Router) {}
 
-  activites = signal<Activity[]>([
+  cards = [
     {
-      name: 'EMPANADAS DE LA IGLESIA',
-      img: 'https://images.pexels.com/photos/22880519/pexels-photo-22880519/free-photo-of-personas-con-ropa-tradicional-y-comida-en-la-mesa-durante-un-festival-tradicional-en-la-ciudad.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'EMPANADAS DE LA IGLESIA',
+      description: 'Descubre tu vocación y planifica tu futuro.',
+      icon: 'https://images.pexels.com/photos/22880519/pexels-photo-22880519/free-photo-of-personas-con-ropa-tradicional-y-comida-en-la-mesa-durante-un-festival-tradicional-en-la-ciudad.jpeg?auto=compress&cs=tinysrgb&w=600',
       link: 'https://comfamiliarhuila.com/recreacion/programa-adulto-mayor/',
     },
     {
-      name: 'JORNADA DE RECICLAJE',
-      img: 'https://images.pexels.com/photos/2682683/pexels-photo-2682683.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'JORNADA DE RECICLAJE',
+      description: 'Mejora tus habilidades sociales y de comunicación.',
+      icon: 'https://images.pexels.com/photos/2682683/pexels-photo-2682683.jpeg?auto=compress&cs=tinysrgb&w=600',
       link: 'https://www.comfamiliar.com/conoce-las-ultimas-novedades-de-nuestro-programa-de-gerontologia/',
     },
     {
-      name: 'LECTURA Y LITERATURA',
-      img: 'https://images.pexels.com/photos/30952073/pexels-photo-30952073/free-photo-of-biblioteca-gabriel-garcia-marquez-barcelona.jpeg?auto=compress&cs=tinysrgb&w=600',
-      link: '',
+      title: 'LECTURA Y LITERATURA',
+      description:
+        'Evalúa tus habilidades y obtén recomendaciones personalizadas.',
+      icon: 'https://images.pexels.com/photos/30952073/pexels-photo-30952073/free-photo-of-biblioteca-gabriel-garcia-marquez-barcelona.jpeg?auto=compress&cs=tinysrgb&w=600',
+      link: '/evaluaciones',
     },
     {
-      name: 'JORNADA DE PLANTACION',
-      img: 'https://images.pexels.com/photos/4503261/pexels-photo-4503261.jpeg?auto=compress&cs=tinysrgb&w=600',
-      link: '',
+      title: 'JORA DE PLANTACION',
+      description:
+        'Evalúa tus habilidades y obtén recomendaciones personalizadas.',
+      icon: 'https://images.pexels.com/photos/4503261/pexels-photo-4503261.jpeg?auto=compress&cs=tinysrgb&w=600',
+      link: '/evaluaciones',
     },
-  ]);
+  ];
 
   goToModule(route: string) {
     this.router.navigate([route]);
